112cccaffc270d8591364231525b7c2948da1ff4
itsoporte <itsoporte@cemaco.com>
2024-10-04 11:40:10 -0600

    conexion testearlo de manera local

---
+++ Prueba_Conexion/Prueba_Conexion/Conexion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+using System.Net;
+
+
+namespace Prueba_Conexion
+{
+    class Conexion
+    {
+        SqlConnection SqlCon;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
+
+        public void Open()
+        {
+            string nombre_servidor = Dns.GetHostName();
+            SqlCon = new SqlConnection("Data Source= "+nombre_servidor+"\\LOCAL;Initial Catalog=PREUBA1;Integrated Security=True;Encrypt=False");
+
+            SqlCon.Open();
+
+
+        }
+
+        public void Close()
+        { 
+            SqlCon.Close();
+        }
+
+        public string Cadena()
+        {
+            return SqlCon.ConnectionString;
+        }
+
+    }
+}
